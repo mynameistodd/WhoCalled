6a7b490ceb0948b82e872ff25e951ec903c26934
Todd DeLand <todd.deland@gmail.com>
2012-05-20 12:55:55 -0400

    Refactor posting code
    Refactor big postWhoCalledResponse to smaller methods for each post to
    whocalled.us, who called heroku, and Facebook.

--- src/com/mynameistodd/whocalled/SubmitResponse.java
+++ src/com/mynameistodd/whocalled/SubmitResponse.java
@@ -1,22 +1,36 @@
 package com.mynameistodd.whocalled;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URLEncoder;
 import java.util.Calendar;
 
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import com.facebook.android.AsyncFacebookRunner;
+import com.facebook.android.AsyncFacebookRunner.RequestListener;
+import com.facebook.android.Facebook;
+import com.facebook.android.FacebookError;
 import com.google.ads.AdRequest;
 import com.google.ads.AdView;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+//import android.net.Credentials;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -34,9 +48,16 @@ public class SubmitResponse extends Activity {
 	private EditText callerIDInput;
 	private TextView who_called_from;
 	String baseURL = "http://whocalled.us/do?action=report&name=test&pass=test&phoneNumber=";
-	String result;
+	String baseMyURL = "http://strong-robot-8518.herokuapp.com/";
+	String baseMyURLJson = baseMyURL + "answers.json";
+	String baseMyURLAnswers = baseMyURL + "answers/";
+	String resultWCUS;
+	String resultWCH;
 	AdView adView;
 	private Util whoCalledUtil;
+	Facebook facebook = new Facebook("206261666149850");
+	AsyncFacebookRunner mAsyncRunner;
+	private SharedPreferences mPrefs;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -65,31 +86,26 @@ public class SubmitResponse extends Activity {
 			@Override
 			public void onClick(View v) {
 				String returnMessage = "";
-				try {
-					returnMessage = postWhoCalledResponse(whoCalledInput.getText().toString(), callerIDInput.getText().toString());
-					
-					if (returnMessage.equals("success=1"))
+				
+				returnMessage = postWhoCalledResponses(whoCalledInput.getText().toString(), callerIDInput.getText().toString());
+				
+				if (returnMessage.equals("success=1"))
+				{
+					setResult(RESULT_OK);
+				}
+				else
+				{
+					Intent returnIntent = new Intent();
+					if (returnMessage.length() > 0)
 					{
-						setResult(RESULT_OK);
-					}
-					else
-					{
-						Intent returnIntent = new Intent();
-						if (returnMessage.length() > 0)
+						String[] split1 = returnMessage.split("&");
+						if (split1.length > 1)
 						{
-							String[] split1 = returnMessage.split("&");
-							if (split1.length > 1)
+							String[] split2 = split1[1].split("=");
+							if (split2.length > 1)
 							{
-								String[] split2 = split1[1].split("=");
-								if (split2.length > 1)
-								{
-									returnIntent.putExtra("com.mynameistodd.whocalled.response", split2[1]);
-									setResult(2, returnIntent);
-								}
-								else
-								{
-									setResult(RESULT_CANCELED);
-								}
+								returnIntent.putExtra("com.mynameistodd.whocalled.response", split2[1]);
+								setResult(2, returnIntent);
 							}
 							else
 							{
@@ -101,21 +117,51 @@ public class SubmitResponse extends Activity {
 							setResult(RESULT_CANCELED);
 						}
 					}
-				} catch (UnsupportedEncodingException e) {
-					e.printStackTrace();
+					else
+					{
+						setResult(RESULT_CANCELED);
+					}
 				}
 				
 				finish();		
 			}
 		});
         
-		
+		mPrefs = getSharedPreferences(Util.PREFERENCES_NAME, MODE_PRIVATE);
+        String access_token = mPrefs.getString("access_token", null);
+        long expires = mPrefs.getLong("access_expires", 0);
+        if(access_token != null) {
+            facebook.setAccessToken(access_token);
+        }
+        if(expires != 0) {
+            facebook.setAccessExpires(expires);
+        }
 	}
 
-	public String postWhoCalledResponse(String whoCalled, String callerID) throws UnsupportedEncodingException{  
-        HttpClient httpclient = new DefaultHttpClient();
-        String encodedWhoCalled = URLEncoder.encode(whoCalled, "UTF-8");
-        String encodedCallerID = URLEncoder.encode(callerID, "UTF-8");
+	public String postWhoCalledResponses(String whoCalled, String callerID) 
+	{
+		String encodedWhoCalled = "";
+        String encodedCallerID = "";
+		try {
+			encodedWhoCalled = URLEncoder.encode(whoCalled, "UTF-8");
+			encodedCallerID = URLEncoder.encode(callerID, "UTF-8");
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		}
+		
+        resultWCUS = postWhoCalledUS(encodedWhoCalled, encodedCallerID);
+        resultWCH = postWhoCalledHeroku(encodedWhoCalled, encodedCallerID);
+        postFacebook(resultWCH);
+        
+        whoCalledUtil.saveToCache(number, whoCalled);
+        
+        return resultWCUS;
+    }
+	
+	public String postWhoCalledUS(String encodedWhoCalled, String encodedCallerID)
+	{
+		HttpClient httpclient = new DefaultHttpClient();
+		ResponseHandler<String> handler = new BasicResponseHandler();
         
         final Calendar now = Calendar.getInstance();
         int year = now.get(Calendar.YEAR);
@@ -125,21 +171,104 @@ public class SubmitResponse extends Activity {
         
         String postArgs = baseURL + number + "&date=" + currentDate + "&callerID=" + encodedCallerID + "&identity=" + encodedWhoCalled + "&postalCode=";
         HttpPost request = new HttpPost(postArgs);
-        ResponseHandler<String> handler = new BasicResponseHandler();  
         try {  
-            result = httpclient.execute(request, handler);  
-            Log.d("mynameistodd", "result: " + result);
+            resultWCUS = httpclient.execute(request, handler);  
+            Log.d("mynameistodd", "result: " + resultWCUS);
         } catch (ClientProtocolException e) {  
             e.printStackTrace();  
         } catch (IOException e) {  
             e.printStackTrace();  
-        }  
+        }
+        
         httpclient.getConnectionManager().shutdown();
+        return resultWCUS;
+	}
+	public String postWhoCalledHeroku(String encodedWhoCalled, String encodedCallerID)
+	{
+		HttpClient httpclient = new DefaultHttpClient();
+		ResponseHandler<String> handler = new BasicResponseHandler();
+		HttpPost request = new HttpPost(baseMyURLJson);
+		
+//		DefaultHttpClient client = new DefaultHttpClient();
+//		Credentials creds = new UsernamePasswordCredentials("todd", "supersecret");
+//		client.getCredentialsProvider().setCredentials(new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT), creds);
         
-        whoCalledUtil.saveToCache(number, whoCalled);
+        JSONObject answer = new JSONObject();
+        try {
+			answer.put("title", encodedWhoCalled);
+			answer.put("phone", number);
+		} catch (JSONException e1) {
+			e1.printStackTrace();
+		}
         
-        return result;
-    }
+        StringEntity s = null;
+		try {
+			s = new StringEntity(answer.toString());
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		}
+        s.setContentType("application/json");
+        
+        request.setEntity(s);
+        
+        try {  
+            resultWCH = httpclient.execute(request, handler);  
+            Log.d("mynameistodd", "result2: " + resultWCH);
+        } catch (ClientProtocolException e) {  
+            e.printStackTrace();  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        }
+        
+        httpclient.getConnectionManager().shutdown();
+        return resultWCH;
+	}
+	public void postFacebook(String herokuResponse)
+	{
+		Log.d("mynameistodd", "Facebook isSessionValid: " + String.valueOf(facebook.isSessionValid()));
+        if (facebook.isSessionValid()) {
+        	
+        	String responseID = "";
+        	try {
+				JSONObject response = new JSONObject(herokuResponse);
+				responseID = response.getString("id");
+			} catch (JSONException e1) {
+				e1.printStackTrace();
+			}
+        	
+        	mAsyncRunner = new AsyncFacebookRunner(facebook);
+        	Bundle parameters = new Bundle();
+        	parameters.putString("access_token", facebook.getAccessToken());
+        	parameters.putString("answer", baseMyURLAnswers + responseID);
+        	mAsyncRunner.request("me/com_mynameistodd_wc:submit", parameters, "POST", new RequestListener() {
+				
+				@Override
+				public void onMalformedURLException(MalformedURLException e, Object state) {
+					e.printStackTrace();
+				}
+				
+				@Override
+				public void onIOException(IOException e, Object state) {
+					e.printStackTrace();
+				}
+				
+				@Override
+				public void onFileNotFoundException(FileNotFoundException e, Object state) {
+					e.printStackTrace();
+				}
+				
+				@Override
+				public void onFacebookError(FacebookError e, Object state) {
+					e.printStackTrace();
+				}
+				
+				@Override
+				public void onComplete(String response, Object state) {
+					Log.d("mynameistodd", response);
+				}
+			}, null);
+        }
+	}
 	
 	@Override
 	protected void onDestroy() {
